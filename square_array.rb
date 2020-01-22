03039ea47a6ce8365fed310b060139d1a5486f09
Roberto Livi <robertomlivi@outlook.com>
2020-01-22 21:59:52 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,9 +1,10 @@
 numbers = [1, 2, 3]
+new_array = []
 def square_array(x)
-  x.each do |index|
-   puts index**2
+  x.each do |new_array|
+    new_array**2
   end
+  return new_array
 end
 
-print square_array(numbers)
-
+square_array(numbers)
